2a7010e039898b79a15f036e8ec3e2df0858be96
Filip Klečka <kleckafilippp@gmail.com>
2024-04-17 18:19:17 +0200

    Desktopová verze bez responsivity

---
+++ script.js
@@ -0,0 +1,26 @@
+// Mobile menu
+
+const hamburgerMenu = document.querySelector("#mobile-menu");
+const mobileNavigation = document.querySelector("#mobile-nav");
+
+hamburgerMenu.addEventListener("click", function(e) {
+  e.preventDefault();
+  hamburgerMenu.classList.toggle("close");
+  if (mobileNav.style.display === "block") {
+    mobileNavigation.style.display = "none";
+  } else {
+    mobileNavigation.style.display = "block";
+  }
+});
+
+// If you click on mobile navigation href, mobile menu disappear
+
+const mobileHref = document.querySelectorAll(".mobile-href");
+const mobileNav = document.getElementById("mobile-nav");
+
+for(let oneHref of mobileHref){
+  oneHref.addEventListener("click", function(){
+    mobileNav.style.display = "none";
+    hamburgerMenu.classList.remove("close")
+  })
+}
